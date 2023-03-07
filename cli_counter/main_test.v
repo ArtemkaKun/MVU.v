79dfa072ac419c9b2ef75546a1bd6218e8c73286
Artem Yurchenko <zumikq78@gmail.com>
2023-03-07 19:34:52 +0100

    Implement: CLI counter tests

---
+++ cli_counter/main_test.v
@@ -0,0 +1,15 @@
+module cli_counter
+
+fn test_update_returns_incremented_model_on_increment() {
+	model := Model{}
+	incremented_model := update(Message.increment, model)
+
+	assert incremented_model.value == 1
+}
+
+fn test_update_returns_decremented_model_on_decrement() {
+	model := Model{}
+	decremented_model := update(Message.decrement, model)
+
+	assert decremented_model.value == -1
+}
